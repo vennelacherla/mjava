f8315ab49f18891645562a28c5866b19af79bdf0
vennelacherla <vennelacherla11@gmail.com>
2023-12-13 12:40:10 +0530

    maven java push

---
+++ src/main/java/kmit_java/mjava/App.java
@@ -0,0 +1,13 @@
+package kmit_java.mjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
